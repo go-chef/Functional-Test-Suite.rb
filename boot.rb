5c1fca925edba9c56e038951032fd224732dbc23
Ranjib Dey <ranjib@pagerduty.com>
2014-10-08 00:50:18 -0700

    generate secret to test encrypted data baf

--- boot.rb
+++ boot.rb
@@ -12,21 +12,18 @@ require 'tempfile'
 require 'chef/providers'
 require 'chef/resources'
 require 'digest/md5'
+require 'mixlib/shellout'
 
 class Machine
-
   attr_reader :ct
-
   def initialize(name)
     @ct = LXC::Container.new(name)
   end
-
   def read_file(path)
     ct.execute do
       File.read(path)
     end
   end
-
   def run_recipe(file)
     recipe_text = ::File.read(file)
     Chef::Config[:solo] = true
@@ -47,15 +44,6 @@ class Machine
       nil
     end
   end
-
-  def client
-    @client ||= Class.new(Chef::Client) do
-      def run_ohai
-        ohai.run_plugins
-      end
-    end.new
-  end
-
   def start
     unless ct.defined?
       ct.create('download', nil, {}, 0, %w{-a amd64 -r trusty -d ubuntu})
@@ -65,7 +53,6 @@ class Machine
       sleep 1 while ct.ip_addresses.empty?
     end
   end
-
   def destroy
     ct.stop if ct.running?
     ct.destroy if ct.defined?
@@ -73,33 +60,59 @@ class Machine
   def knife_config
 <<EOF
 chef_server_url 'http://#{ct.ip_addresses.first}:4646'
-client_name 'admin'
+node_name 'admin'
 client_key '../keys/admin.pem'
 EOF
   end
 
-  def setup
+  private
+  def client
+    @client ||= Class.new(Chef::Client) do
+      def run_ohai
+        ohai.run_plugins
+      end
+    end.new
+  end
+end
+module Helper
+  extend self
+  def install_server
+    server =  Machine.new('goiardi')
+    server.start
+    server.run_recipe('recipes/goiardi.rb')
+    sleep 20
+    clean
+    write_configs(server)
+  end
+  def write_configs(server)
     FileUtils.mkdir_p('keys')
     FileUtils.mkdir_p('etc')
     %w{admin.pem  chef-validator.pem}.each do |key|
       File.open("keys/#{key}", 'w') do |f|
-        f.write(read_file("etc/goiardi/#{key}"))
+        f.write(server.read_file("/etc/goiardi/#{key}"))
       end
     end
+    File.open('keys/secret', 'w') do |f|
+      f.write(generate_secret)
+    end
     File.open('etc/knife.rb', 'w') do |f|
-      f.write(knife_config)
+      f.write(server.knife_config)
     end
   end
   def clean
     FileUtils.rm_rf('keys')
     FileUtils.rm_rf('etc')
   end
+  def generate_secret
+    cmd = Mixlib::ShellOut.new("openssl rand -base64 512 | tr -d '\r\n'")
+    cmd.run_command
+    fail cmd.stderr unless cmd.exitstatus.zero?
+    cmd.stdout
+  end
+  def uninstall_server
+    server =  Machine.new('goiardi')
+    server.destroy
+  end
 end
-
-machine =  Machine.new('goiardi')
-machine.clean
-machine.start
-machine.run_recipe('recipes/goiardi.rb')
-sleep 3
-machine.setup
-#machine.destroy
+Helper.install_server
+#Helper.uninstall_server
