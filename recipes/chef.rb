d8cbbfbbcbfe0a46bca1d74b590c1263fc7ce196
Ranjib Dey <ranjib@pagerduty.com>
2015-01-14 09:07:07 -0800

    install ssh server. newer containers dont have them by default

--- recipes/chef.rb
+++ recipes/chef.rb
@@ -1,8 +1,11 @@
 
 execute 'apt-get update -y'
+package 'openssh-server'
+
+execute 'echo ubuntu:ubuntu | chpasswd'
 
 remote_file '/tmp/chef.deb' do
-  source 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/13.04/x86_64/chef_11.16.4-1_amd64.deb'
+  source 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/13.04/x86_64/chef_12.0.3-1_amd64.deb'
   mode 0644
   action :create_if_missing
 end
